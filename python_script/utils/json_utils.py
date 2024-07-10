7fddafd92293c2eaa7d22c09ea78fb4931ca72b6
idenrai <geniuskwon966@gmail.com>
2024-07-10 17:25:55 +0900

    add generator type default

--- python_script/utils/json_utils.py
+++ python_script/utils/json_utils.py
@@ -1,16 +1,19 @@
 import json
 from decimal import Decimal
+from types import GeneratorType
 
 
-def decimal_default(obj):
+def custom_default(obj):
     if isinstance(obj, Decimal):
         return float(obj)
+    if isinstance(obj, GeneratorType):
+        return list(obj)
     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
 
 def save_to_json(file_path, data):
     with open(file_path, 'w') as json_file:
-        json.dump(data, json_file, default=decimal_default)
+        json.dump(data, json_file, default=custom_default)
 
 
 def load_from_json(file_path):
